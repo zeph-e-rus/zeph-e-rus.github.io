ef7118b6cc3ef88c4e42a611c1e8cf2ba8dd7a6c
arpit-sat <arpitsatnalika.tv@gmail.com>
2020-02-13 15:07:21 +0530

    Update job.js

--- js/job.js
+++ js/job.js
@@ -8,36 +8,26 @@ function myFunction() {
     document.getElementById("myDropdown2").classList.toggle("show");
   }
   // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
+    window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content1");
+      var dropdowns1 = document.getElementsByClassName("dropdown-content1");
+      var dropdowns2 = document.getElementsByClassName("dropdown-content2");
+      var dropdowns3 = document.getElementsByClassName("dropdown-content3");
       var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+      for (i = 0; i < dropdowns1.length; i++) {
+        var openDropdown = dropdowns1[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
       }
-    }
-  }
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content2");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+      for (i = 0; i < dropdowns2.length; i++) {
+        var openDropdown = dropdowns2[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
       }
-    }
-  }
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content3");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+      for (i = 0; i < dropdowns3.length; i++) {
+        var openDropdown = dropdowns3[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
