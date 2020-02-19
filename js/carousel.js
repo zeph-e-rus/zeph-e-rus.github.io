06a8e81b5e8a4711ab043fc1ed7f8e4b49a38848
arpit-sat <arpitsatnalika.tv@gmail.com>
2020-02-19 14:56:33 +0530

    Added paper card design

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,26 @@
+var x1 =document.getElementById("one");
+var x2 =document.getElementById("two");
+var x3 =document.getElementById("three");
 function one() {
     document.getElementById("demo").innerHTML = "The seeds were sown for Task Venturers back in November 2017, when a batch of digital nomads were finding repeated success in all aspects of their work due to their dedication, hard work, high standards and the obligation to deliver quality. Since then, over the past couple of years, we have managed to grow from a group of digital freelancers into a global powerhouse of Virtual Assistance skills, with a more updated, profound and innovative skillset.<br>Now every day, our people are developing and presenting innovative virtual business solutions, which supports the business transformation of our clients.<br>We expanded and stepped in to help the taskmasters with a wide range of Virtual Assistant (VA) services, encompassing niches like web development, SEO, digital marketing, graphic designing, e-commerce, content writing, video editing, and virtual accounting. In this mission, we have with us the most talented, hardworking and dedicated team of experts. They understand that each business is unique and obtain a deep understanding of your customers, market, competition, challenges, and future strategy.<br>We evolved into a team of experts who spend their days supporting and innovating the bedrock of our business – you.";
-    }
+    var x =document.getElementById("one");
+    x1.style.color="#FF790C";
+    x2.style.color="black";
+    x3.style.color="black";
+  }
     
     function two() {
     document.getElementById("demo").innerHTML = "Our mission is to enable the business owners to focus on their most important priorities, save time and achieve an appropriate work-life balance. Quality, punctuality, commitment, and honesty is our motto. We take, organize and simplify the huge volume of administrative tasks that steal the valuable time of business owners. We ensure the ultimate business growth and profitability of our clients.";
-      
+    x2.style.color="#FF790C";
+    x1.style.color="black";
+    x3.style.color="black";
     }
     
     function three() {
     document.getElementById("demo").innerHTML = "Our vision is to become the number one dedicated, efficient, viable and friendly Virtual Assistant business proudly helping business owners to plan and implement innovative ideas and strategies for ultimate growth and success that enable them to live their dream lifestyle of freedom-based business. We aim to be the global leader in the virtual assistance industry that delivers the highest quality with dedication, timeliness, a sense of warmth and team spirit.";
-    
+    x3.style.color="#FF790C";
+    x2.style.color="black";
+    x1.style.color="black";
     }
     var x = document.getElementById("content").innerHTML;
     var y = document.getElementById("content1").innerHTML;
--- js/faq.js
+++ js/faq.js
@@ -1,46 +1,51 @@
 var first_click = true;
-
 function faq1() {
+  var ab =document.getElementById('one');
+  var icon = document.getElementById('ab1');
     if (first_click)
     {
-    document.getElementById("one").innerHTML = "<br>You can choose any of the plans to use our services. If you want to choose a custom quote for a specific service, then we prefer to go through a Memorandum of Understanding or MOU to secure both ends.";
-    var icon = document.getElementById('ab1')
+    ab.innerHTML = "You can choose any of the plans to use our services. If you want to choose a custom quote for a specific service, then we prefer to go through a Memorandum of Understanding or MOU to secure both ends.";
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
-        document.getElementById("one").innerHTML ="";
-        var icon = document.getElementById('ab1')
+        ab.innerHTML ="";
+       ab.classList.remove("paper-card3");
         if (icon.classList.contains('fa-minus-circle'))
          {
           icon.classList.remove('fa-minus-circle');
           icon.classList.add('fa-plus-circle');
         }
     first_click = true;
+    
         }
 }
-var first_click = true;
 
+var first_click = true;
 function faq2() {
+  var ab =document.getElementById('two');
     if (first_click)
     {
-    document.getElementById("two").innerHTML = "<br>We always believe in ease of access and deliverability. That’s why we make all our services available to you as long as you have hours available in your account.";
+    document.getElementById("two").innerHTML = "We always believe in ease of access and deliverability. That’s why we make all our services available to you as long as you have hours available in your account.";
     var icon = document.getElementById('ab2')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("two").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab2')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -53,20 +58,23 @@ function faq2() {
 var first_click = true;
 
 function faq3() {
+  var ab =document.getElementById('three');
     if (first_click)
     {
-    document.getElementById("three").innerHTML = "<br>Yes, of course. Our services are available 24×7. You can simply let your assigned assistant know which time slot works for you.";
+    document.getElementById("three").innerHTML = "Yes, of course. Our services are available 24×7. You can simply let your assigned assistant know which time slot works for you.";
     var icon = document.getElementById('ab3')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("three").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab3')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -79,20 +87,23 @@ function faq3() {
 var first_click = true;
 
 function faq4() {
+  var ab =document.getElementById('four');
     if (first_click)
     {
-    document.getElementById("four").innerHTML = "<br>Delivering quality tasks is our motto. We make sure you are introduced to two suited assistants with whom you are able to go on a call to discuss the scope of your work. The purpose of assigning two dedicated assistants is to make sure if one of your dedicated assistants goes on leave or falls sick, there’s always someone else to take over your work. ";
+    document.getElementById("four").innerHTML = "Delivering quality tasks is our motto. We make sure you are introduced to two suited assistants with whom you are able to go on a call to discuss the scope of your work. The purpose of assigning two dedicated assistants is to make sure if one of your dedicated assistants goes on leave or falls sick, there’s always someone else to take over your work. ";
     var icon = document.getElementById('ab4')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("four").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab4')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -105,20 +116,23 @@ function faq4() {
 var first_click = true;
 
 function faq5() {
+  var ab =document.getElementById('five');
     if (first_click)
     {
-    document.getElementById("five").innerHTML = "<br>If you are no longer willing to continue with our services you can unsubscribe at any time. Just send us an email at info@taskventurers.com and you will be unsubscribed as soon as we get your email. However, it doesn’t ban you from returning back to our services whenever you want to.";
+    document.getElementById("five").innerHTML = "If you are no longer willing to continue with our services you can unsubscribe at any time. Just send us an email at info@taskventurers.com and you will be unsubscribed as soon as we get your email. However, it doesn’t ban you from returning back to our services whenever you want to.";
     var icon = document.getElementById('ab5')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("five").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab5')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -131,20 +145,23 @@ function faq5() {
 var first_click = true;
 
 function faq6() {
+  var ab =document.getElementById('six');
     if (first_click)
     {
-    document.getElementById("six").innerHTML = "<br>You will have the flexibility of getting in touch with your Virtual Assistant through any mode of communication you prefer. You can use a phone, Skype, social media sites or any messenger service for interacting with your Virtual Assistant. However, your assistant will only be available during their regular shift timings unless they are requested to be available during some other time slot.";
+    document.getElementById("six").innerHTML = "You will have the flexibility of getting in touch with your Virtual Assistant through any mode of communication you prefer. You can use a phone, Skype, social media sites or any messenger service for interacting with your Virtual Assistant. However, your assistant will only be available during their regular shift timings unless they are requested to be available during some other time slot.";
     var icon = document.getElementById('ab6')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("six").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab6')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -157,20 +174,23 @@ function faq6() {
 var first_click = true;
 
 function faq7() {
+  var ab =document.getElementById('seven');
     if (first_click)
     {
-    document.getElementById("seven").innerHTML = "<br>We always believe in ease of access and deliverability. That’s why we make all our services available to you as long as you have hours available in your account.";
+    document.getElementById("seven").innerHTML = "We always believe in ease of access and deliverability. That’s why we make all our services available to you as long as you have hours available in your account.";
     var icon = document.getElementById('ab7')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("seven").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab7')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -183,20 +203,23 @@ function faq7() {
 var first_click = true;
 
 function faq8() {
+  var ab =document.getElementById('eight');
     if (first_click)
     {
-    document.getElementById("eight").innerHTML = "<br>We always believe in ease of access and deliverability. That’s why we make all our services available to you as long as you have hours available in your account.";
+    document.getElementById("eight").innerHTML = "We always believe in ease of access and deliverability. That’s why we make all our services available to you as long as you have hours available in your account.";
     var icon = document.getElementById('ab8')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("eight").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab8')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -209,20 +232,23 @@ function faq8() {
 var first_click = true;
 
 function faq9() {
+  var ab =document.getElementById('nine');
     if (first_click)
     {
-    document.getElementById("nine").innerHTML = "<br>Our system recognizes only the email addresses that are subscribed to our website. Thus, mails received from only the subscribed email addresses are authorized to enter our CRM. Mails from unrecognized addresses go to the unrecognized section and are automatically flushed off by our system.";
+    document.getElementById("nine").innerHTML = "Our system recognizes only the email addresses that are subscribed to our website. Thus, mails received from only the subscribed email addresses are authorized to enter our CRM. Mails from unrecognized addresses go to the unrecognized section and are automatically flushed off by our system.";
     var icon = document.getElementById('ab9')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("nine").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab9')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -235,20 +261,23 @@ function faq9() {
 var first_click = true;
 
 function faq10() {
+  var ab =document.getElementById('ten');
     if (first_click)
     {
-    document.getElementById("ten").innerHTML = "<br>We use PayPal as our primary payment gateway. However, in order to avoid any inconvenience, we also accept payments using other payment gateways through custom invoicing. Get in touch with our sales and support team to learn more about our custom invoicing.";
+    document.getElementById("ten").innerHTML = "We use PayPal as our primary payment gateway. However, in order to avoid any inconvenience, we also accept payments using other payment gateways through custom invoicing. Get in touch with our sales and support team to learn more about our custom invoicing.";
     var icon = document.getElementById('ab10')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("ten").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab10')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -261,20 +290,23 @@ function faq10() {
 var first_click = true;
 
 function faq11() {
+  var ab =document.getElementById('eleven');
     if (first_click)
     {
-    document.getElementById("eleven").innerHTML = "<br>If you are not willing to continue your subscription with us, please request cancellation 72 hours prior to your next account renewal date. In case of cancellation with any remaining hours in your account, we will refund 85% valuation of the hours in your account due to transaction charges involved.";
+    document.getElementById("eleven").innerHTML = "If you are not willing to continue your subscription with us, please request cancellation 72 hours prior to your next account renewal date. In case of cancellation with any remaining hours in your account, we will refund 85% valuation of the hours in your account due to transaction charges involved.";
     var icon = document.getElementById('ab11')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("eleven").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab11')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -287,20 +319,23 @@ function faq11() {
 var first_click = true;
 
 function faq12() {
+  var ab =document.getElementById('twelve');
     if (first_click)
     {
-    document.getElementById("twelve").innerHTML = "<br>All our payments are received via PayPal only. Sign up for a PayPal account and you’re good to go. In case you want to use any other payment method, you can contact our sales team or drop an email at info@taskventurers.com.";
+    document.getElementById("twelve").innerHTML = "All our payments are received via PayPal only. Sign up for a PayPal account and you’re good to go. In case you want to use any other payment method, you can contact our sales team or drop an email at info@taskventurers.com.";
     var icon = document.getElementById('ab12')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("twelve").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab12')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -313,20 +348,23 @@ function faq12() {
 var first_click = true;
 
 function faq13() {
+  var ab =document.getElementById('thirteen');
     if (first_click)
     {
-    document.getElementById("thirteen").innerHTML = "<br>We believe in preserving the quality of our work through consistency and coherence. In order to avoid discrepancies that may appear due to the insufficient hours left in your account, you’re automatically subscribed to your chosen plan once the free trial ends.";
+    document.getElementById("thirteen").innerHTML = "We believe in preserving the quality of our work through consistency and coherence. In order to avoid discrepancies that may appear due to the insufficient hours left in your account, you’re automatically subscribed to your chosen plan once the free trial ends.";
     var icon = document.getElementById('ab13')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("thirteen").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab13')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -339,20 +377,23 @@ function faq13() {
 var first_click = true;
 
 function faq14() {
+  var ab =document.getElementById('fourteen');
     if (first_click)
     {
-    document.getElementById("fourteen").innerHTML = "<br>Hours go in accordance with the plans you choose to go with and are subject to renewal within the stipulated time frame (mostly 30 days from the commencement date of the plan). Thus, we request our valued clientele to utilize their hours within the time frame of their chosen plan.";
+    document.getElementById("fourteen").innerHTML = "Hours go in accordance with the plans you choose to go with and are subject to renewal within the stipulated time frame (mostly 30 days from the commencement date of the plan). Thus, we request our valued clientele to utilize their hours within the time frame of their chosen plan.";
     var icon = document.getElementById('ab14')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("fourteen").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab14')
         if (icon.classList.contains('fa-minus-circle'))
          {
@@ -365,20 +406,23 @@ function faq14() {
 var first_click = true;
 
 function faq15() {
+  var ab =document.getElementById('fifteen');
     if (first_click)
     {
-    document.getElementById("fifteen").innerHTML = "<br>The privacy and security of your data are of utmost importance to us. We deal with confidential information pertaining to our valued clientele. That is why our website is protected with multi-layered security parameters to prevent any sort of data theft or phishing attacks. All your data is stored in our database, which is highly secure and protected. To ensure security at the next level, we make sure all our employees sign a Non-Disclosure Agreement prior to joining.";
+    document.getElementById("fifteen").innerHTML = "The privacy and security of your data are of utmost importance to us. We deal with confidential information pertaining to our valued clientele. That is why our website is protected with multi-layered security parameters to prevent any sort of data theft or phishing attacks. All your data is stored in our database, which is highly secure and protected. To ensure security at the next level, we make sure all our employees sign a Non-Disclosure Agreement prior to joining.";
     var icon = document.getElementById('ab15')
     if (icon.classList.contains('fa-plus-circle'))
      {
       icon.classList.remove('fa-plus-circle');
       icon.classList.add('fa-minus-circle');
+      ab.classList.add("paper-card3");
     }
     first_click = false;
     }
     else
         {
         document.getElementById("fifteen").innerHTML ="";
+       ab.classList.remove("paper-card3");
         var icon = document.getElementById('ab15')
         if (icon.classList.contains('fa-minus-circle'))
          {
