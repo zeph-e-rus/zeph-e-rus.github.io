af0887b84588b9c56c86e5c46ffa72881bc70aef
arpit-sat <arpitsatnalika.tv@gmail.com>
2020-02-20 15:43:39 +0530

    added go-to-top button

--- js/pop-up.js
+++ js/pop-up.js
@@ -10,3 +10,23 @@
         $(function(){
             $("#includedContent").load("login.html"); 
           });
+          
+ //top - button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
